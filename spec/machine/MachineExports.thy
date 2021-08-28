83a9117758b7c2ec73b0206b43b8ed53ffbc20a8
Michael McInerney <m.mcinerney@unsw.edu.au>
2021-08-28 18:22:38 +1000

    rt machine: add to requalify_consts and requalify_facts
    Signed-off-by: Michael McInerney <m.mcinerney@unsw.edu.au>

--- spec/machine/MachineExports.thy
+++ spec/machine/MachineExports.thy
@@ -67,32 +67,40 @@ requalify_consts
   timerPrecision
   max_time
   getCurrentTime_buffer
+  factor1
+  factor2
 
 requalify_facts
-  us_to_ticks_mono
-  us_to_ticks_zero
-  us_to_ticks_nonzero
-  kernelWCET_ticks_no_overflow
   MAX_PERIOD_US_def
   MAX_PERIOD_def
-  us_to_ticks_mult
-  getCurrentTime_buffer_no_overflow
   kernelWCET_ticks_def
   replicate_no_overflow
-  getCurrentTime_buffer_nonzero
   getCurrentTime_buffer_nonzero'
   getCurrentTime_buffer_no_overflow'
   getCurrentTime_buffer_no_overflow'_stronger
   getCurrentTime_buffer_minus
   getCurrentTime_buffer_minus'
   MAX_PERIOD_mult
-  MAX_PERIOD_mult'
+  factor1_non_zero
+  MIN_BUDGET_bound
+  getCurrentTime_buffer_bound
+  kernelWCET_pos'
+  MIN_BUDGET_pos'
+  domain_time_pos
+  getCurrentTime_buffer_pos
+  unat_max_word
+  unat_add_lem''
+  unat_mult_lem'
+  us_to_ticks_def
+  getCurrentTime_buffer_no_overflow
+  us_to_ticks_mono
+  MIN_BUDGET_helper
 
 definition "MAX_RELEASE_TIME = max_time - 5 * MAX_PERIOD"
 
 lemma unat_MAX_RELEASE_TIME:
   "unat MAX_RELEASE_TIME = unat max_time - 5 * unat MAX_PERIOD"
-  apply (clarsimp simp: MAX_RELEASE_TIME_def unat_sub MAX_PERIOD_mult')
+  apply (clarsimp simp: MAX_RELEASE_TIME_def unat_sub MAX_PERIOD_mult)
   done
 
 (* HERE IS THE PLACE FOR GENERIC WORD LEMMAS FOR ALL ARCHITECTURES *)
