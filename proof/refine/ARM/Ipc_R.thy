fd915c4dcdf707a352f6daa11b7dceedd08588a0
Michael McInerney <m.mcinerney@unsw.edu.au>
2022-01-19 23:00:16 +1100

    rt refine: remove lemmas and definitions now in earlier sessions
    Signed-off-by: Michael McInerney <m.mcinerney@unsw.edu.au>

--- proof/refine/ARM/Ipc_R.thy
+++ proof/refine/ARM/Ipc_R.thy
@@ -1944,8 +1944,6 @@ lemma possibleSwitchTo_valid_queues[wp]:
 lemma cancelAllIPC_valid_queues':
   "cancelAllIPC t \<lbrace> valid_queues' \<rbrace>"
   apply (clarsimp simp: cancelAllIPC_def)
-  apply (fold restartThreadIfNoFault_def)
-  apply (fold cancelAllIPC_loop_body_def)
   apply (wpsimp wp: mapM_x_wp' get_ep_inv' getEndpoint_wp)
   done
 
@@ -2057,23 +2055,6 @@ lemma bind_sc_reply_invs[wp]:
    apply fastforce
   apply (erule (1) valid_objs_sc_replies_distinct)
   done
-
-(* FIXME RT: move to AInvs *)
-crunches bind_sc_reply
-  for pspace_distinct[wp]: pspace_distinct
-  and pspace_aligned[wp]: pspace_aligned
-  and active_sc_valid_refills[wp]: active_sc_valid_refills
-  (ignore: update_sched_context)
-
-lemma bind_sc_reply_valid_objs[wp]:
-  "\<lbrace>valid_objs and reply_at reply_ptr and sc_at sc_ptr and
-    sc_replies_sc_at (\<lambda>a. reply_ptr \<notin> set a) sc_ptr\<rbrace>
-   bind_sc_reply sc_ptr reply_ptr
-   \<lbrace>\<lambda>_. valid_objs\<rbrace>"
-  unfolding bind_sc_reply_def
-  apply (wpsimp wp: hoare_list_all_lift)
-  apply (drule (1) valid_objs_ko_at)
-  by (clarsimp simp: valid_obj_def valid_sched_context_def sc_at_pred_n_def obj_at_def)
 
 lemma replyPush_corres:
   "can_donate = can_donate' \<Longrightarrow>
@@ -2329,12 +2310,6 @@ lemma tcbEPFindIndex_inv[wp]:
 
 crunches tcbEPAppend
   for ep_at'[wp]: "ep_at' epptr"
-
-(* FIXME RT: move to AInvs *)
-crunches reply_push
-  for pspace_aligned[wp]: pspace_aligned
-  and pspace_distinct[wp]: pspace_distinct
-  (wp: crunch_wps simp: crunch_simps)
 
 crunches bindScReply
   for typ_at'[wp]: "\<lambda>s. P (typ_at' T p s)"
--- proof/refine/ARM/Tcb_R.thy
+++ proof/refine/ARM/Tcb_R.thy
@@ -337,11 +337,6 @@ lemma asUser_postModifyRegisters_corres:
   apply (rule corres_stateAssert_assume)
    by simp+
 
-(* FIXME RT: move *)
-crunches Tcb_A.restart
-  for ex_nonz_cap_to[wp]: "ex_nonz_cap_to tcb_ptr"
-  (wp: crunch_wps cancel_ipc_cap_to simp: crunch_simps)
-
 crunches restart
   for ex_nonz_cap_to'[wp]: "ex_nonz_cap_to' tcbPtr"
   (wp: crunch_wps threadSet_cap_to simp: crunch_simps tcb_cte_cases_def)
@@ -1582,25 +1577,6 @@ where
                                           and bound_tcb_at' ((=) None) t) )"
 | "tcb_inv_wf' (tcbinvocation.SetTLSBase ref w)
              = (tcb_at' ref and ex_nonz_cap_to' ref)"
-
-(* FIXME RT: move to a more appropriate place *)
-lemma cap_delete_deletes_fh:
-  "\<lbrace>\<lambda>s. p \<noteq> ptr \<longrightarrow> cte_wp_at valid_fault_handler ptr s \<and>
-                     cte_wp_at (\<lambda>c. P c \<or> c = cap.NullCap) p s\<rbrace>
-   cap_delete ptr
-   \<lbrace>\<lambda>rv. cte_wp_at (\<lambda>c. P c \<or> c = cap.NullCap) p\<rbrace>, -"
-  apply (rule_tac Q'="\<lambda>rv s. ((p = ptr) \<longrightarrow> cte_wp_at (\<lambda>c. P c \<or> c = cap.NullCap) p s) \<and>
-                             ((p \<noteq> ptr) \<longrightarrow> cte_wp_at (\<lambda>c. P c \<or> c = cap.NullCap) p s)"
-               in hoare_post_imp_R)
-   apply (rule hoare_vcg_precond_impE_R)
-    apply (rule hoare_vcg_conj_lift_R)
-     apply (rule hoare_post_imp_R[OF cap_delete_deletes])
-     apply (clarsimp simp: cte_wp_at_def)
-    apply (rule hoare_vcg_const_imp_lift_R)
-    apply (rule cap_delete_ep)
-   apply simp
-  apply clarsimp
-  done
 
 lemma installTCBCap_corres_helper:
   "n \<in> {0,1,3,4} \<Longrightarrow>
